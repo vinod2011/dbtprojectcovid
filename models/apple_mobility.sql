02f504aacc7f59360e2608b9c2e3c13f6e6f5879
alapati.kumar@coforge.com <alapati.kumar@coforge.com>
2026-04-09 09:34:23 +0000

    Created New view

---
+++ models/apple_mobility.sql
@@ -0,0 +1,11 @@
+{{ config(
+    materialized = 'view'
+) }}
+
+select
+    country_region,
+    province_state,
+    date as cdate,
+    transportation_type
+from COVID.COVID19.APPLE_MOBILITY
+where country_region = 'France';
