86011453d6cac01c92c156aeb05562ef465baef9
Vinod Alapati <alapati.kumar@coforge.com>
2026-06-26 15:33:25 +0000

    Table

---
+++ models/COVID19/TBL_POLICY_MEASURES.sql
@@ -0,0 +1,23 @@
+{{
+    config(
+        materialized='incremental',
+        incremental_strategy='insert_overwrite',
+        database='TARGET_DB',
+        schema='COVID19',
+        alias='TBL_POLICY_MEASURES'
+    )
+}}
+
+SELECT
+    STATE_ID,
+    COUNTY,
+    FIPS_CODE,
+    POLICY_LEVEL,
+    POLICY_TYPE,
+    START_STOP,
+    COMMENTS,
+    TOTAL_PHASE,
+    LAST_UPDATE_DATE,
+    LAST_REPORTED_FLAG,
+    CURRENT_TIMESTAMP() AS ETL_LOADDATETIME
+FROM COVID.PUBLIC.CDC_POLICY_MEASURES
